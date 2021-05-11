726ec299e5d6ac9ff644e29cdb0229384459be9a
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2021-05-11 16:53:13 +0800

    deplanner

--- include/cerlab_uav/deplanner.h
+++ include/cerlab_uav/deplanner.h
@@ -1,9 +1,11 @@
 #ifndef DEPLANNER_H
 #define DEPLANNER_H
 #include <ros/ros.h>
+#include <cerlab_uav/prm.h>
 #include <nav_msgs/Odometry.h>
 #include <octomap_msgs/Octomap.h>
 #include <octomap_msgs/conversions.h>
+
 
 using std::cout; using std::endl;
 
@@ -12,6 +14,9 @@ private:
 	ros::NodeHandle nh;
 	ros::Subscriber odom_sub;
 	ros::Subscriber octomap_sub;
+	Node current_node; // used as current position for planning
+	OcTree* tree_ptr; // current map for planning
+
 
 public:
 	deplanner(ros::NodeHandle _nh);
