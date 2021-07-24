bd425a1c211e00d003ec67e959ed6d0c499632ee
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2021-07-24 13:18:41 +0800

    trajectory following

--- src/offb_trajectory.cpp
+++ src/offb_trajectory.cpp
@@ -32,10 +32,10 @@ int main(int argc, char **argv)
             ("mavros/state", 10, state_cb);
     ros::Subscriber odom_sub = nh.subscribe<nav_msgs::Odometry>
             ("mavros/local_position/odom", 10, odom_cb);
-    // ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-    //         ("mavros/setpoint_position/local", 10);
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("cerlab_uav/goal", 10);
+            ("mavros/setpoint_position/local", 10);
+    // ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+    //         ("cerlab_uav/goal", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
             ("mavros/cmd/arming");
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
@@ -71,14 +71,14 @@ int main(int argc, char **argv)
 
     std::string filename = "/home/zhefan/catkin_ws/src/trajectory_optimization/path/waypoint_maze_complete.txt";
     std::vector<std::vector<pose>> paths = read_waypoint_file(filename);
-    std::vector<pose> path = paths[30];
+    std::vector<pose> path = paths[38];
 
 
     // parameters
     double res = 0.1; // map resolution
     double xsize = 0.2; double ysize = 0.2; double zsize = 0.1; // Robot collision box size
     int degree = 7; // polynomial degree
-    double velocityd = 2; // desired average velocity
+    double velocityd = 1; // desired average velocity
     int diff_degree = 4; // Minimum snap (4), minimum jerk (3)
     double perturb = 1; // Regularization term and also make PSD -> PD
     bool shortcut = true; // shortcut waypoints
@@ -92,7 +92,7 @@ int main(int argc, char **argv)
     int horizon = 20; // MPC horizon
 
     double mass = 1.0; double k_roll = 1.0; double tau_roll = 1.0; double k_pitch = 1.0; double tau_pitch = 1.0; 
-    double T_max = 3 * mass * 9.8; double roll_max = PI_const/6; double pitch_max = PI_const/6; double yawdot_max = PI_const/3;
+    double T_max = 2 * 9.8; double roll_max = PI_const/6; double pitch_max = PI_const/6; double yawdot_max = PI_const/3;
     mpcPlanner mp (horizon);
     mp.loadParameters(mass, k_roll, tau_roll, k_pitch, tau_pitch);
     mp.loadControlLimits(T_max, roll_max, pitch_max, yawdot_max);
@@ -159,9 +159,9 @@ int main(int argc, char **argv)
         rpy_from_quaternion(quat, current_roll, current_pitch, current_yaw);
 
        
-        currentStates(0) = current_x; currentStates(1) = current_y; currentStates(2) = current_z;
-        currentStates(3) = current_vx; currentStates(4) = current_vy; currentStates(5) = current_vz;
-        currentStates(6) = current_roll; currentStates(7) = current_pitch; currentStates(8) = current_yaw;
+        // currentStates(0) = current_x; currentStates(1) = current_y; currentStates(2) = current_z;
+        // currentStates(3) = 0; currentStates(4) = 0; currentStates(5) = 0;
+        // currentStates(6) = current_roll; currentStates(7) = current_pitch; currentStates(8) = current_yaw;
 
         std::vector<pose> mpc_trajectory;
 
@@ -201,21 +201,23 @@ int main(int argc, char **argv)
 
 
             mp.optimize(currentStates, nextStates, mpc_trajectory);
-            // currentStates(0) = nextStates(0); currentStates(1) = nextStates(1); currentStates(2) = nextStates(2);
-            // currentStates(3) = nextStates(3); currentStates(4) = nextStates(4); currentStates(5) = nextStates(5); 
-            // currentStates(6) = nextStates(6); currentStates(7) = nextStates(7); currentStates(8) = nextStates(8);  
+            // currentStates(0) = current_x; currentStates(1) = current_y; currentStates(2) = current_z;
+            currentStates(0) = nextStates(0); currentStates(1) = nextStates(1); currentStates(2) = nextStates(2);
+            currentStates(3) = nextStates(3); currentStates(4) = nextStates(4); currentStates(5) = nextStates(5); 
+            currentStates(6) = nextStates(6); currentStates(7) = nextStates(7); currentStates(8) = nextStates(8);  
+            cout << nextStates << endl;
 
             if (traj_idx <= trajectory.size() - 1){
-                pose_target.pose.position.x = trajectory[traj_idx].x;
-                pose_target.pose.position.y = trajectory[traj_idx].y;
-                pose_target.pose.position.z=  trajectory[traj_idx].z;
-                geometry_msgs::Quaternion target_quat = quaternion_from_rpy(0, 0, trajectory[traj_idx].yaw);
-                // pose_target.pose.position.x = mpc_trajectory[1].x;
-                // pose_target.pose.position.y = mpc_trajectory[1].y;
-                // pose_target.pose.position.z = mpc_trajectory[1].z;
-                // geometry_msgs::Quaternion target_quat = quaternion_from_rpy(0, 0, mpc_trajectory[1].yaw);
+                // pose_target.pose.position.x = trajectory[traj_idx].x;
+                // pose_target.pose.position.y = trajectory[traj_idx].y;
+                // pose_target.pose.position.z = trajectory[traj_idx].z;
+                // geometry_msgs::Quaternion target_quat = quaternion_from_rpy(0, 0, trajectory[traj_idx].yaw);
+                pose_target.pose.position.x = mpc_trajectory[1].x;
+                pose_target.pose.position.y = mpc_trajectory[1].y;
+                pose_target.pose.position.z = mpc_trajectory[1].z;
+                geometry_msgs::Quaternion target_quat = quaternion_from_rpy(nextStates(6), nextStates(7), nextStates(8));
                 pose_target.pose.orientation = target_quat;
-                ++traj_idx;
+                // ++traj_idx;
             }
             
         }
